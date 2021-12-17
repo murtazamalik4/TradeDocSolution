ad34ba76dd6082c3ad88ce8686ee0269704a81d2
murtazamalik4 <murtazamalik78643@gmail.com>
2021-12-16 22:26:23 -0800

    commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -833,92 +833,9 @@
                 <div class="bounce3"></div>
             </div>
         </div>
-        <header id="ct-masthead">
-            <div id="ct-header-wrap" class="ct-header-layout1 fixed-height is-sticky ">
-               
-                <div id="ct-header-middle">
-                    <div class="container">
-                        <div class="row">
-                            <div class="ct-header-branding"> 
-                                
-                                </div>
-                            <div class="ct-header-holder">
-                                <div class="ct-header-info-item ct-header-call">
-                                    <div class="h-item-icon"> <i class="flaticon-telephone text-gradient"></i></div>
-                                    <div class="h-item-meta"> <label>Call Us: (00971) 5246-94830</label> <span>(Mon - Thurs)</span></div> <a href="tel:+123456789" target="_self" class="h-item-link"></a></div>
-                                <div class="ct-header-info-item ct-header-mail">
-                                    <div class="h-item-icon"> <i class="flaticonv3-envelope text-gradient"></i></div>
-                                    <div class="h-item-meta"> <label>Mail us for help:</label> <span>info@tradedocsolution.com</span></div> <a href="mailto:info@consultio.com" target="_self" class="h-item-link"></a></div>
-                               
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div id="ct-header" class="ct-header-main">
-                    <div class="container">
-                        <div class="row">
-                            <div class="ct-header-branding"> <a class="logo-web5" href="/" title="Consultio" rel="home"><img src="/wp-content/uploads/2020/02/logo-web.png" alt="Consultio"/></a>
-                            
-                                   </div>
-                            <div class="ct-header-navigation">
-                                <div class="ct-header-navigation-bg">
-                                    <nav class="ct-main-navigation">
-                                        <div class="ct-main-navigation-inner">
-                                           
-                                            <div class="header-mobile-search">
-                                                <form role="search" method="get" action="/"> <input type="text" placeholder="Search..." name="s" class="search-field" /> <button type="submit" class="search-submit"><i class="fac fac-search"></i></button></form>
-                                            </div>
-                                            <ul id="menu-main-menu" class="ct-main-menu sub-hover children-plus clearfix">
-                                                <li id="menu-item-3911" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3911">
-                                                    <a href="/" class="show-demos">
-                                                        <span>Home</span>
-                                                    </a>
-                                                </li>
-
-                                                <li id="menu-item-42" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-42"><a href="/lc-docs"><span>LC</span></a>
-                                                    
-                                                </li>
-                                                <li id="menu-item-414" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-414"><a href="/credit-insurance"><span>Trade Credit Insurance</a>
-                                                   
-                                                </li>
-
-                                                <li id="menu-item-414" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-414"><a href="/about"><span>About</a>
-                                                   
-                                                </li>
-
-                                                <li id="menu-item-414" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-414"><a href="/collection-doc.html"><span>Contact</a>
-                                                   
-                                                </li>
-                                                
-                                            
-                                               
-                                              
-                                            </ul>
-                                            <div class="ct-header-holder ct-header-holder-mobile">
-                                                <div class="ct-header-info-item ct-header-call">
-                                                    <div class="h-item-icon"> <i class="flaticon-telephone text-gradient"></i></div>
-                                                    <div class="h-item-meta"> <label>Call Us: (00971) 5246-94830</label> <span>(Mon - Thurs)</span></div> <a href="tel:+123456789" target="_self" class="h-item-link"></a></div>
-                                                <div class="ct-header-info-item ct-header-mail">
-                                                    <div class="h-item-icon"> <i class="flaticonv3-envelope text-gradient"></i></div>
-                                                    <div class="h-item-meta"> <label>Mail us for help:</label> <span>info@lcdocsolution.com</span></div> <a href="mailto:info@consultio.com" target="_self" class="h-item-link"></a></div>
-                                                <div class="ct-header-info-item ct-header-address">
-                                                    <div class="h-item-icon"> <i class="flaticon-map text-gradient"></i></div>
-                                                    <div class="h-item-meta"> <label>380 St Kilda Road,</label> <span>Melbourne, Australia</span></div> <a href="https://www.google.com/maps/place/380+St+Kilda+Rd,+Melbourne+VIC+3000,+Australia/" target="_blank" class="h-item-link"></a></div>
-                                            </div>
-                                            <div class="ct-header-button-mobile" style="background:linear-gradient(#3ac4f0 0%,#00a9df 100%) !important;"> <a class="btn btn-default" href="https://dashboard.tradedocsolutions/sign-up" target="_self">Register Now</a></div>
-                                        </div>
-                                    </nav>
-                                   
-                                </div>
-                            </div>
-                            <div class="ct-header-button" style="background:linear-gradient(#3ac4f0 0%,#00a9df 100%) !important;"> <a class="btn btn-default" href="https://dashboard.tradedocsolution.com/sign-up" target="_self">Register Now<i class="ct-arrow-right arrow-white"></i></a></div>
-                        </div>
-                    </div>
-                    <div id="ct-menu-mobile"><span class="btn-nav-mobile open-menu"> <span></span> </span>
-                    </div>
-                </div>
-            </div>
-        </header>
+        
+        @include('layout.header') 
+        
         <div id="content" class="site-content">
             <div class="content-inner">
                 <div class="container content-container">
